b5738225e852f79531abdd8c4567c16dbc1e2486
Leif Denby <leif@denby.eu>
2021-07-19 14:20:58 +0200

    add merged crops

--- dyamond/pipeline/crop.py
+++ dyamond/pipeline/crop.py
@@ -9,7 +9,11 @@ from .cdoalt import cdo
 
 GRID_FILES_PATH = Path("/work/ka1081/DYAMOND/PostProc/GridsAndWeights")
 
-MODEL_GRIDFILE_IDS = {"ICON-5km": "ICON_R2B09_2"}
+MODEL_GRIDFILE_IDS = {
+    "ICON-5km": "ICON_R2B09_2",
+    "UM-5km": "UM-5km",
+    "GOES-3km": "GEOS-3.25km"
+}
 
 
 def _make_model_gridfile_path(resolution, model):
@@ -31,13 +35,13 @@ class CropDyamondFile(luigi.Task):
     grid if needed)
     """
 
-    date = luigi.DateParameter(default=datetime.datetime(year=2020, month=2, day=23))
-    model = luigi.Parameter(default="ICON-5km")
-    time_resolution = luigi.Parameter(default="15min")
-    variable = luigi.Parameter(default="rlut")
+    date = luigi.DateParameter()
+    model = luigi.Parameter()
+    time_resolution = luigi.Parameter()
+    variable = luigi.Parameter()
     bbox = luigi.ListParameter()
 
-    dst_grid_latlon_resolution = luigi.FloatParameter()
+    dst_grid_latlon_resolution = luigi.FloatParameter(default=None)
 
     src_data_path_root = luigi.Parameter(default=dyamond_data.DATA_ROOT_MISTRAL)
     dst_data_path_root = luigi.Parameter()
@@ -61,53 +65,61 @@ class CropDyamondFile(luigi.Task):
 
         Path(p_out).parent.mkdir(exist_ok=True, parents=True)
 
-        grid_filename = _make_latlon_gridfile_path(
-            resolution=self.dst_grid_latlon_resolution
-        )
-        weights_filename = _make_model_gridfile_path(
-            resolution=self.dst_grid_latlon_resolution, model=self.model
-        )
+        cdo_pl = cdo(verbose=True).selname(self.variable, infile=str(p_in))
 
-        cdo_pl = cdo(verbose=True).selname(self.variable)
-        if self.model in MODEL_GRIDFILE_IDS:
+        if self.dst_grid_latlon_resolution is not None:
+            grid_filename = _make_latlon_gridfile_path(
+                resolution=self.dst_grid_latlon_resolution
+            )
+            weights_filename = _make_model_gridfile_path(
+                resolution=self.dst_grid_latlon_resolution, model=self.model
+            )
+            if self.model not in MODEL_GRIDFILE_IDS:
+                raise NotImplementedError(self.model)
             cdo_pl = cdo_pl.remap(grid_filename, weights_filename)
-        cdo_pl.sellonlatbox(*list(self.bbox)).execute(p_in, p_out)
+
+        cdo_pl.sellonlatbox(*list(self.bbox)).execute(p_out)
 
     def output(self):
         resolution = self.dst_grid_latlon_resolution
-        crop_id = f"{resolution:.02f}x{resolution:.02f}deg"
+        if resolution is None:
+            res_id = "native_grid"
+        else:
+            res_id = f"{resolution:.02f}x{resolution:.02f}deg"
+
         bbox_id = "".join([f"{d}{v:.02f}" for (d, v) in zip("WESN", list(self.bbox))])
 
-        dset_id = f"{crop_id}_{bbox_id}"
+        dset_id = f"{res_id}_{bbox_id}"
 
-        f_path = dyamond_data.make_path(
-            date=self.date,
-            model=self.model,
-            time_resolution=self.time_resolution,
-            variable=self.variable,
-            data_root=Path(self.dst_data_path_root) / dset_id,
-        )
+        import ipdb
+        with ipdb.launch_ipdb_on_exception():
+            f_path = dyamond_data.make_path(
+                date=self.date,
+                model=self.model,
+                time_resolution=self.time_resolution,
+                variable=self.variable,
+                data_root=Path(self.dst_data_path_root) / dset_id,
+            )
 
         return luigi.LocalTarget(f_path)
 
 
-class CropDyamondFiles(CropDyamondFile):
+class CropDyamondFiles(luigi.Task):
     start_date = luigi.DateParameter()
     end_date = luigi.DateParameter()
     variables = luigi.Parameter()
 
-    model = luigi.Parameter(default="ICON-5km")
-    time_resolution = luigi.Parameter(default="15min")
+    model = luigi.Parameter()
+    time_resolution = luigi.Parameter()
     bbox = luigi.ListParameter()
 
-    dst_grid_latlon_resolution = luigi.FloatParameter()
+    dst_grid_latlon_resolution = luigi.FloatParameter(default=None)
 
     src_data_path_root = luigi.Parameter(default=dyamond_data.DATA_ROOT_MISTRAL)
     dst_data_path_root = luigi.Parameter()
 
     def requires(self):
         tasks = []
-
         for variable in list(self.variables):
             date = self.start_date
             while date <= self.end_date:
@@ -124,3 +136,6 @@ class CropDyamondFiles(CropDyamondFile):
                 tasks.append(task)
                 date += datetime.timedelta(days=1)
         return tasks
+
+    def output(self):
+        return self.input()
